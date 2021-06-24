0928daa4a8601124712d708503c5d4efac2a5bac
gabriel <innermond@yahoo.com>
2021-06-24 08:54:01 +0300

    filter sheets by index and/or by source

--- down.py
+++ down.py
@@ -17,13 +17,14 @@ def load_keys():
   return keys
 
 
-def rows_from(excel):
+def rows_from(excel, ii=[-1], key_only=None):
   try:
       book = xlrd.open_workbook(excel)
-      names = book.sheet_names()
-      for name in names:
-          print('sheet name {}'.format(name))
-          sh = book.sheet_by_name(name)
+      if ii == [-1]:
+          ii = range(0, book.nsheets)
+      for i in ii:
+          sh = book.sheet_by_index(i)
+          print('sheet name {}'.format(sh.name))
           for rowx in range(sh.nrows):
               url = sh.row(rowx)[1] #assumed 1 exists
               if  not url.value:
@@ -38,7 +39,9 @@ def rows_from(excel):
 
 # get list
 input = argparse.ArgumentParser(description="Get a PDF's list and download them")
-input.add_argument('-l', '--list', type=str, help='list filepath')
+input.add_argument('-l', '--list', type=str, required=True, help='list filepath')
+input.add_argument('-o', '--only', type=int, nargs='+', default=[-1], help="use only sheets selected by position, first sheet is 0")
+input.add_argument('-s', '--source', type=str, nargs='+', default=None, help='use only sources indicated, must be top domain - ieee.org, not explore.ieee.org')
 args = input.parse_args()
 if args is None:
   raise Exception('no arguments provided')
@@ -46,13 +49,15 @@ if args is None:
 # keys
 try:
   keys = load_keys()
+  if args.source is not None:
+    keys = {k:v for k, v in keys.items() if k in args.source}
 except Exception as err:
   print('could not load keys', err) 
   sys.exit(1)
  
 # open excel
 try:
-  for url in rows_from(args.list):
+  for url in rows_from(args.list, args.only):
     # here we start to scrape
     print(f'trying to download from: {url}')
 except FileNotFoundError as err:
