144699d54fd7ca67bca59ca12ce300745db4be6a
gabriel <innermond@yahoo.com>
2021-08-10 01:38:16 +0300

    get text from pdf

--- pdf_search.py
+++ pdf_search.py
@@ -1,5 +1,12 @@
-import logging
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfparser import PDFParser
+import io
 import os
+import logging
 
 from input_pdf_search import arguments
 
@@ -13,4 +20,24 @@ ch.setFormatter(formatter)
 log.addHandler(ch)
 
 if __name__ == '__main__':
-  print(arguments)
+  fp = open(arguments.pdf, 'rb')
+  rsrcmgr = PDFResourceManager()
+  retstr = io.StringIO()
+  codec = 'utf-8'
+  laparams = LAParams()
+  device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+  interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+  page_no = 0
+  for pageNumber, page in enumerate(PDFPage.get_pages(fp)):
+    interpreter.process_page(page)
+    data = retstr.getvalue()
+    data = data.encode('utf-8')
+    print(data, page_no)
+    #with open(os.path.join('Files/Company_list/0010/text_parsed/2017AR', f'pdf page {page_no}.txt'), 'wb') as file:
+     # file.write(data.encode('utf-8'))
+    data = ''
+    retstr.truncate(0)
+    retstr.seek(0)
+
+    page_no += 1
